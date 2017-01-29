0477ceee8861c30cf27b05e2c28963ef455e1739
Ahmed M. Ammar <ahmed3mar@outlook.com>
2017-01-30 00:11:13 +0200

    Oki

--- index.php
+++ index.php
@@ -6,5 +6,16 @@ $script = file_get_contents("js/app.js");
 $css = str_replace('@charset "UTF-8";', "", $css);
 $html = str_replace("{style}", $css, $html);
 $html = str_replace("{script}", $script, $html);
-
+echo $html;
 file_put_contents("index.html", $html);
+/*
+sass scss/style.scss:scss/style.css
+grunt
+
+server
+
+git add -A
+git commit -m "Oki"
+git push
+
+*/
