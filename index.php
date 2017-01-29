1b70a0bab7712124510fe0fb431fdadedda39232
Ahmed M. Ammar <ahmed3mar@outlook.com>
2017-01-29 19:18:09 +0200

    Hello

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $html = file_get_contents("index-min.html");
 $css = file_get_contents("css/style.css");
 $script = file_get_contents("js/app.js");
 $css = str_replace('@charset "UTF-8";', "", $css);
-$html = str_replace("{style", $css, $html);
-$html = str_replace("{script", $script, $html);
+$html = str_replace("{style}", $css, $html);
+$html = str_replace("{script}", $script, $html);
 
 file_put_contents("index.html", $html);
